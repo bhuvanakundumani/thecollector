3b3e4c736149d7762aa5c0c9139a51ce35a5cf72
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2021-02-04 03:23:01 +0330

    r5
    - Add a a guideline to the site.
    - Make template titles editable in themselves.
    - Remove ALL comments and extra stuff.

--- thecollector/routes.py
+++ thecollector/routes.py
@@ -1,19 +1,10 @@
-from thecollector import App, DB, relative
+from thecollector import App
 from thecollector.forms import DataForm
-from thecollector.models import Data
-from flask import (
-    render_template,
-    flash,
-    redirect,
-    send_from_directory,
-    url_for,
-)
-from time import strftime, gmtime
-import json
+from flask import render_template, flash
 
 
 @App.route("/", methods=["GET", "POST"])
-def root():
+def form():
     form = DataForm()
     if form.validate_on_submit():
         form.commit(nullify=True)
@@ -21,16 +12,6 @@ def root():
     return render_template("form.html", form=form)
 
 
-# @App.route("/dump")
-# def dump():
-#     fname = "data_" + strftime("%y%m%d-%H%M%S", gmtime()) + ".json"
-#     fpath = relative("static/data/" + fname)
-#     data = [i.to_dict() for i in Data.query.all()]
-#     with open(fpath, "w") as f:
-#         json.dump(data, f)
-#     return redirect(url_for("send_data", path=fname))
-
-
-# @App.route("/static/data/<path:path>")
-# def send_data(path):
-#     return send_from_directory("static/data", path, as_attachment=True)
+@App.route("/guideline")
+def guideline():
+    return render_template("guideline.html")
